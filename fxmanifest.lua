93d5d921b4c96b53ba383f1dd237e993ff9c1ba8
Azusakawa <58968500+azusakawa@users.noreply.github.com>
2021-07-27 17:42:00 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Azusakawa & MuraYuki'
+description 'FiveM Global Ban System'
+version '2.1'
+
+server_only 'yes'
+
+server_scripts {
+	'Main/azk_GlobalBan.net.dll'
+}
